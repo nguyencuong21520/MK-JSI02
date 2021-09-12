3634d164373f29687567963704c0df24bf7d61b1
nguyencuong21520 <58180566+nguyencuong21520@users.noreply.github.com>
2021-09-12 11:15:11 +0700

    chat

--- lesson5_register/chat.js
+++ lesson5_register/chat.js
@@ -1,14 +1,113 @@
 window.onload = init;
 
 async function init() {
-    firebase.auth().onAuthStateChanged((user)=>{
-        if(user && user.emailVerified){
-            loadConversations(user.email);
-        }else{
-            alert("Please signin")
-            open("./index.html", "_self");
-        }
-    })
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user && user.emailVerified) {
+      document.querySelector("#currentEmail").innerHTML = user.email
+      loadConversations(user.email);
+      conversationsChange(user.email)
+    } else {
+      alert("Please signin");
+      open("./index.html", "_self");
+    }
+  });
+}
+
+let renderChat = (data, email) => {
+  let dom = document.querySelector(".chat-content-container");
+  let chat_name = document.querySelector("#chat_name");
+  let chat_ID = document.querySelector("#currentID");
+
+  chat_name.innerHTML = data.chatName;
+  chat_ID.innerHTML = data.id;
+
+  dom.innerHTML = "";
+  for (let i = 0; i < data.messages.length; i++) {
+    let chat_class = "chat-content";
+    if (data.messages[i].owner == email) {
+      chat_class = "chat-content owner";
+    }
+    let html = `<div class="${chat_class}">
+    <span>${data.messages[i].content}</span>
+  </div>`;
+
+    dom.innerHTML += html;
+  }
+};
+
+let renderListFriends = (data, email) => {
+  let dom = document.querySelector("#list_friends");
+  dom.innerHTML = "";
+  for (let i = 0; i < data.length; i++) {
+    let html = `<div id="c${data[i].id}" class="list-group-item list-group-item-action lh-tight">
+    <div>
+      <img src="https://img.icons8.com/material-outlined/48/000000/user-male-circle.png">
+      <span>${data[i].chatName}</span>
+    </div>
+
+    <div class="time">
+      <span>${data[i].createAt}</span>
+    </div>
+  </div>`;
+    dom.innerHTML += html;
+  }
+
+  for (let i of data) {
+    let user = document.getElementById(`c${i.id}`);
+    user.onclick = () => {
+      renderChat(i, email);
+    };
+  }
+};
+
+let form = document.querySelector("#sent_message");
+form.onsubmit = (e) => {
+  e.preventDefault();
+
+  let message = form.m.value;
+  let id = document.querySelector("#currentID").textContent;
+  let email = document.querySelector("#currentEmail").textContent;
+
+  updateMessage(message,id,email)
+
+  form.m.value = ""
+};
+
+let updateMessage = async (content, id, email) => {
+  if (id) {
+    let m = {
+      content: content,
+      owner: email,
+    };
+
+    await firebase
+      .firestore()
+      .collection("chat")
+      .doc(id)
+      .update({
+        messages: firebase.firestore.FieldValue.arrayUnion(m),
+      });
+  } else {
+    alert("something wrong");
+  }
+};
+
+let conversationsChange = async (email) =>{
+  firebase.firestore().collection("chat").where("users", "array-contains", email)
+  .onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+          if (change.type === "added") {
+              // console.log("them moi: ", change.doc.data());
+          }
+          if (change.type === "modified") {
+
+            renderChat(change.doc.data(), email)
+          }
+          if (change.type === "removed") {
+              // console.log("Removed city: ", change.doc.data());
+          }
+      });
+  });
 }
 
 // CRUD
@@ -16,7 +115,6 @@ async function init() {
 // create,
 // update,`
 // delete
-
 
 let signOut = () => {
   firebase
@@ -40,6 +138,8 @@ let loadConversations = async (email) => {
 
   let conversations = getDataFromDocs(result.docs);
   console.log(conversations);
+  renderChat(conversations[0], currentUser);
+  renderListFriends(conversations, currentUser);
 };
 
 const sweetAlert = (icon, title) => {
